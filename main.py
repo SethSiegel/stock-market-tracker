ec77eb3ea8a12eed511d766604fbbb0218f19897
SethSiegel <sasiegel3@g.ucla.edu>
2025-07-13 15:11:18 -0700

    separated out functions to their own files for each plotting type

--- main.py
+++ main.py
@@ -1,33 +1,13 @@
-import yfinance as yf
-import plotly.graph_objects as go
+
+import candlestick_plotting as cp
+import multi_ticker_plotting as mtp
 # import pandas as pd
 # from datetime import datetime
 
-ticker = "VOO"
+if __name__ == "__main__":
 
-df = yf.download(tickers=ticker, period='1d', interval='1m',
-                 ignore_tz=True)
-df.columns = ['Close', 'High', 'Low', 'Open', 'Volume']
-df = df.reset_index()
+    tickers = ["AAPL", "VOO", "MSFT", "GOOGL", "AMZN"]
+    ticker = tickers[-1:]
 
-dfTime = df['Datetime'].dt.strftime('%H:%M')
-
-fig = go.Figure(data=[go.Candlestick(
-    x=dfTime,
-    open=df['Open'],
-    high=df['High'],
-    low=df['Low'],
-    close=df['Close'],
-    name='APPLE Live Market Data')])
-
-fig.update_layout(
-    xaxis_rangeslider_visible=False,
-    title='AAPL Live Market Data',
-    xaxis_title='Time (minutes)',
-    yaxis_title='Price (USD)',
-    template='plotly_dark')
-
-fig.show()
-
-
-# print(dfTime)
+    cp.candlestick_plot(ticker)
+    mtp.multi_ticker_plot(tickers)
